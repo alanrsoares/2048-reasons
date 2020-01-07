969b641d18cd34cf3fc60c994a0b68e488ab20a6
Alan Soares <alanrsoars@gmail.com>
2020-01-08 09:06:47 +1300

    Updates

---
+++ precache-manifest.f0d743927a8b9ed7de2f11534c59be4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6da17adf27971b9b6fb",
+    "url": "/2048-reasons/static/js/runtime~main.d6da17ad.js"
+  },
+  {
+    "revision": "3f4e03a95513057f3018",
+    "url": "/2048-reasons/static/js/main.3f4e03a9.chunk.js"
+  },
+  {
+    "revision": "e5d42495dc70adb5fd55",
+    "url": "/2048-reasons/static/js/1.e5d42495.chunk.js"
+  },
+  {
+    "revision": "3f4e03a95513057f3018",
+    "url": "/2048-reasons/static/css/main.d699eade.chunk.css"
+  },
+  {
+    "revision": "172c3a0b765aebcbf504a3725168c9d9",
+    "url": "/2048-reasons/index.html"
+  }
+];
